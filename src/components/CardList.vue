79fc854505f610640c034e7de7ba7bded0f6b251
tom lee <tdroid99@gmail.com>
2018-07-26 08:00:47 -0600

    first commit

---
+++ src/components/CardList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <Card v-for="dragon in dragons" :key='dragon.id' :dragon="dragon"/>
+    </div>
+</template>
+
+<script>
+import Card from './Card'
+export default {
+    name: "CardList",
+    components: { 
+        Card
+        },
+data(){
+    return {
+        dragons: null
+        }
+    },
+    methods: { 
+        fetchDragons() {
+            fetch('../../static/dragons.json')
+                .then((response) => response.json())
+                .then((response) => this.dragons = response.dragons)
+                .then(() => console.log(this.dragons, 'data'))
+                
+        }
+
+    },
+    mounted() {
+        this.fetchDragons() 
+        // this.$nextTick()
+        
+    }
+}
+
+</script>
+
+<style>
+
+</style>
